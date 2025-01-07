01fa5d74835096f5f96489371949d53c47e5c6f3
Bryan Ngongeh <149550695+bryandino673@users.noreply.github.com>
2025-01-07 14:57:13 +0100

    Update cart-cookie.ts

--- src/utils/cart-cookie.ts
+++ src/utils/cart-cookie.ts
@@ -4,15 +4,13 @@ const CART_COOKIE_NAME = "cart-cookies";
 
 /**
  * Set cart data in cookies
- * @param cart - The cart data to save
  */
 export function setCartCookie(cart: any) {
-  Cookies.set(CART_COOKIE_NAME, JSON.stringify(cart), { expires: 7, path: "/" }); // Expires in 7 days
+  Cookies.set(CART_COOKIE_NAME, JSON.stringify(cart), { expires: 7, path: "/" }); 
 }
 
 /**
  * Get cart data from cookies
- * @returns The cart data or a default empty structure
  */
 export function getCartCookie() {
   const cart = Cookies.get(CART_COOKIE_NAME);
