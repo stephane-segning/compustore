27f4d96e365e063609b5a5502b7c954867167148
kouamschekina <kouamdelphine82@gmail.com>
2025-01-08 08:46:19 +0100

    feat(frontend): added 'use client' to support the client side functionality

--- next.config.js
+++ next.config.js
@@ -8,6 +8,17 @@ import compose from 'next-compose-plugins';
 import optimizedImages from 'next-optimized-images';
 
 /** @type {import('next').NextConfig} */
-const config = {};
+const config = {
+    images: {
+        domains: [
+          'images.pexels.com',
+          'm.media-amazon.com',
+          's.alicdn.com',
+          'assets2.razerzone.com',
+        'encrypted-tbn0.gstatic.com',
+          'picsum.photos'
+        ],
+      },
+};
 
 export default compose([optimizedImages], config);
