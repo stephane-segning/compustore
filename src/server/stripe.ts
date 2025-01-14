855718320582db71d41157bd8ca2816694ab67e2
Boris <borisngha6@gmail.com>
2025-01-14 11:06:27 +0100

    feat: initialized the stripe client

--- src/server/stripe.ts
+++ src/server/stripe.ts
@@ -1,5 +1,4 @@
 import Stripe from 'stripe'
-import * as process from "node:process";
 
 const stripe = new Stripe(process.env.STRIPE_PUBLISHABLE_KEY as string, {
     apiVersion: '2024-12-18.acacia'
