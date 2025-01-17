0e2693e0f0d4b38f6082b79d9424be2ae8cc3efb
Boris <borisngha6@gmail.com>
2025-01-17 12:55:32 +0100

    fix: stoped using the process.env

--- src/server/stripe.ts
+++ src/server/stripe.ts
@@ -1,6 +1,8 @@
 import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_PUBLISHABLE_KEY as string, {
+import {env} from '@cps/env'
+
+export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
     apiVersion: '2024-12-18.acacia'
 })
 
