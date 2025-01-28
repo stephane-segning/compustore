c5e618cc29f33b52be6d052ca307477a279c480c
kouamschekina <kouamdelphine82@gmail.com>
2025-01-28 16:28:50 +0100

    refactor: removed unnecessarry code

--- src/server/s3/client.ts
+++ src/server/s3/client.ts
@@ -13,18 +13,4 @@ const createS3Client = () => new S3Client({
   region: env.AWS_REGION, // Specify the region
 });
 
-// export async function uploadToS3(bucketName: string, file: Buffer, fileName: string) {
-//   try {
-//     const command = new PutObjectCommand({
-//       Bucket: bucketName,
-//       Key: fileName,
-//       Body: file,
-//     });
-//     await client.send(command);
-//     console.log('File uploaded successfully');
-//   } catch (err) {
-//     console.error('Error uploading file:', err);
-//     throw err;
-//   }
-// }
 export const client = createS3Client();
